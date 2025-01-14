4cf090d2ea654308aa72f8349b984871fdb5866a
Ziauddin Saeed <117837460+Zia-Saeed@users.noreply.github.com>
2025-01-14 22:39:43 +0500

    updates buildspec and appspec file

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #pull the docker image from docker hub
-docker pull ziasaeeditgeek/blog-website
+docker pull ziasaeeditgeek/full-fledge-blog-website
 
 # run the docker image as container
-docker run -d -p 5000:5000 ziasaeeditgeek/blog-website 
+docker run -d -p 5000:5000 ziasaeeditgeek/full-fledge-blog-website
