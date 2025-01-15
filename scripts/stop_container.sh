a9b1a8ef915d7599498377a54a0633b0813337c8
Ziauddin Saeed <117837460+Zia-Saeed@users.noreply.github.com>
2025-01-15 14:34:21 +0500

    updated stop container file

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # stop the running container (if any)
 
-containerId = `docker ps | awk -f " " '{print $1}'`
+# containerId = `docker ps | awk -f " " '{print $1}'`
 
-docker rm -f $containerId
+# docker rm -f $containerId
